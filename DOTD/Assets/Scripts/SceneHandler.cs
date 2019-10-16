1fd3eb0c2964d6520652493e9475965ececbebbb
cxb6159 <cxb6159@IGM2000-09>
2019-10-16 12:23:49 -0400

    - Added Prefabs folder - added character test object - added new script for SceneHandler      - Have not coded, because intellisense wasn't working

---
+++ DOTD/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneHandler : MonoBehaviour
+{
+    // Public Variables
+    public Object button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (true)
+        {
+
+        }
+    }
+}
