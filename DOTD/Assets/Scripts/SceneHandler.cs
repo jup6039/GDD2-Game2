56aa831c54f6e4ae5ea3105739390b397290de54
coletrain9903 <coleboggan@hotmail.com>
2019-10-17 05:20:21 -0400

    Updated Interation Scene
    - Got buttons to work
    - Got names to change based on who's talking

--- DOTD/Assets/Scripts/SceneHandler.cs
+++ DOTD/Assets/Scripts/SceneHandler.cs
@@ -7,30 +7,37 @@ public class SceneHandler : MonoBehaviour
 {
     // Public Variables
 
-     // local public variables
+     // local variables
     public GameObject button1;                          // holds the first choice button
     public GameObject button2;                          // holds the second choice button
-    private Text choice1;
-    private Text choice2;
-    public GameObject textObject;                       // holds the dialogue object, to edit the text
-    public Text text;                                   // holds the text object in the dialogueText var
-    private int scene = 1;
+    private Text choice1;                               // text for button1
+    private Text choice2;                               // text for button2
 
-    public List<string> dialogue;                  // List of dialogue lines
+    public GameObject dialogueObject;                   // holds the dialogue object, to edit the text
+    private Text text;                                  // holds the text object in the dialogueObject var
+
+    public GameObject nameObject;                       // holds the name object, to edit the text
+    private Text nameText;                              // holds the text object in the nameObject var
+
+    public List<string> dialogue;                       // List of dialogue lines
     private int line;                                   // number of dialogue
 
 
     // static variables
-    static string character = "superman";                            // holds the character you are interacting with
+    static string character = "superman";               // holds the character you are interacting with
     static Dictionary<string, int> interactionScene;    // holds the scene that character is currently in
 
+    private int scene = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         // get text
-        text = textObject.GetComponent<Text>();
+        text = dialogueObject.GetComponent<Text>();
         text.text = dialogue[0];
+
+        nameText = nameObject.GetComponent<Text>();
+        nameText.text = "";
 
         // get choices
         choice1 = button1.GetComponentInChildren<Text>();
@@ -55,34 +62,68 @@ public class SceneHandler : MonoBehaviour
             switch (scene)
             {
                 case 1:
-                    Debug.Log("In here");
                     switch (line)
                     {
+                        case 2:
+                            text.text = dialogue[line];
+                            nameText.text = "Superman Jones";
+                            CheckClick();
+                            break;
+                        case 4:
+                            text.text = dialogue[line];
+                            nameText.text = "Superman Jones";
+                            CheckClick();
+                            break;
                         case 5:
-                            Debug.Log("In Here");
-                            button1.SetActive(true);
+                            nameText.text = "";
+                            //button1.SetActive(true);
+                            text.text = " ";
                             choice1.text = "Point out the plastic bag Superman is failing to hide.";
-                            button2.SetActive(true);
+                            //button2.SetActive(true);
                             choice2.text = "Ask how Naruto is doing.";
                             CheckChoiceSelect(6, 21);
                             break;
                         case 7:
-                            button1.SetActive(true);
+                            nameText.text = "";
+                            //button1.SetActive(true);
+                            text.text = " ";
                             choice1.text = "Ask why he's obviously lying to you.";
-                            button2.SetActive(true);
+                            //button2.SetActive(true);
                             choice2.text = "He's obviously lying, but maybe you shouldn't push it. Talk about something else.";
                             CheckChoiceSelect(7, 15);
                             break;
+                        case 10:
+                            text.text = dialogue[line];
+                            nameText.text = "Superman Jones";
+                            CheckClick();
+                            break;
+                        case 12:
+                            text.text = dialogue[line];
+                            nameText.text = "Superman Jones";
+                            CheckClick();
+                            break;
+                        case 16:
+                            text.text = dialogue[line];
+                            nameText.text = "Superman Jones";
+                            CheckClick();
+                            break;
+                        case 18:
+                            text.text = dialogue[line];
+                            nameText.text = "Superman Jones";
+                            CheckClick();
+                            break;
                         case 22:
-                            button1.SetActive(true);
+                            nameText.text = "";
+                            //button1.SetActive(true);
+                            text.text = " ";
                             choice1.text = "Comment on how good of a brother Superman is.";
-                            button2.SetActive(true);
+                            //button2.SetActive(true);
                             choice2.text = "Ask about Superman's Halloween costume.";
                             CheckChoiceSelect(23, 26);
                             break;
                         default:
                             text.text = dialogue[line];
-
+                            nameText.text = "";
                             CheckClick();
                             break;
                     }
@@ -101,18 +142,26 @@ public class SceneHandler : MonoBehaviour
         }
     }
 
-    void CheckChoiceSelect(int choice1, int choice2)
+    void CheckChoiceSelect(int _choice1, int _choice2)
     {
-        if (Input.GetMouseButtonDown(0) && Input.mousePosition.x < button2.transform.position.x)
+        if (Input.GetMouseButtonDown(0) && Input.mousePosition.x < 0)
         {
-            line = choice1;
+            line = _choice1;
+
+            choice1.text = "";
+            choice2.text = "";
+            nameText.text = "Superman Jones";
         }
-        else if (Input.GetMouseButtonDown(0) && Input.mousePosition.x > button2.transform.position.x)
+        else if (Input.GetMouseButtonDown(0) && Input.mousePosition.x > 0)
         {
-            line = choice2;
+            line = _choice2;
+
+            choice1.text = "";
+            choice2.text = "";
+            nameText.text = "Superman Jones";
         }
 
-        button1.SetActive(false);
-        button2.SetActive(false);
+        /*button1.SetActive(false);
+        button2.SetActive(false);*/
     }
 }
